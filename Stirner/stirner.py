99913898f5a88aaa570698337e19639bf24cf8bd
aves8848 <88733645+aves8848@users.noreply.github.com>
2024-09-25 16:37:44 +0000

    [+] Fix von Noteneingabe bei FSP und Semesternoten; [+] Berechnen von Gesamtnote; [-] Alte Überprüfung von korrekten Eingaben in Notenanfrage(User Input);

--- Stirner/stirner.py
+++ Stirner/stirner.py
@@ -9,20 +9,35 @@ def main():
         "Mathematik": {"semesternote": 0.0, "schriftlich": 0.0, "muendlich": 0.0, "gesamt": 0.0},
         "Physik": {"semesternote": 0.0, "schriftlich": 0.0, "muendlich": 0.0, "gesamt": 0.0},
         "Informatik": {"semesternote": 0.0, "schriftlich": 0.0, "muendlich": 0.0, "gesamt": 0.0}}
-    while True:
-        try:
-            for fach in noten:
-                noten[fach]["semesternote"] = float(input(f"Note im Fach {fach}: "))
-            n = 0
-            while n != 1:
-                fsp_geschrieben = input("In welchen Fächer haben sie FSP geschrieben? ").replace(" ", "").split(",")
-                for fach in fsp_geschrieben:
-                    if isinstance(fach, str) and fach in [fach for fach in noten] and len(fsp_geschrieben) == 3:
-                        noten[fach]["schriftlich"] = float(input(f"Schriftliche FSP im Fach {fach}: "))
-                        n = 1
-            print(berechnen(noten))
-        except ValueError:
-            print("Die Note soll eine Zahl sein.")
+    
+    for fach in noten:
+        while True:
+            try:
+                semesternote = float(input(f"Note im Fach {fach}: "))
+                if 1.0 <= semesternote <= 4.0:
+                    noten[fach]["semesternote"] = semesternote
+                    break
+                else: 
+                    raise ValueError
+            except ValueError:
+                print("Die Note soll eine Zahl zwischen 1 und 4 sein.")
+    n = 0
+    while n != 1:
+        fsp_geschrieben = input("In welchen Fächer haben sie FSP geschrieben? ").replace(" ", "").split(",")
+        for fach in fsp_geschrieben:
+            if isinstance(fach, str) and fach in [fach for fach in noten] and len(fsp_geschrieben) == 3:
+                while True:
+                    try:
+                        schriftlichenote = float(input(f"Schriftliche FSP im Fach {fach}: "))
+                        if 1.0 <= schriftlichenote <= 4.0:
+                            noten[fach]["schriftlich"] = schriftlichenote
+                            break
+                        else: 
+                            raise ValueError
+                    except ValueError:
+                        print("Die Note soll eine Zahl zwischen 1 und 4 sein.")
+                n = 1
+    print(berechnen(noten))
         
 
 if __name__ == "__main__":
