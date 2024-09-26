fd32fa0041da608a9eeb9f6625bd3520ee719355
aves8848 <88733645+aves8848@users.noreply.github.com>
2024-09-26 08:23:36 +0000

    [+] Neue Funktion für die Berechnung von mündlichen Noten [+] Abschneiden von Noten von z.B. 4.46 zu 4.4 [+] Fix von Eingabencheck bei FSP-Prüfungsnoten

--- Stirner/stirner.py
+++ Stirner/stirner.py
@@ -1,22 +1,40 @@
-def muendliche_berechnen(faecher, notentabelle):
-    return
-        
+import math
 
-def berechnen(notentabelle):
+
+def truncate(f):
+    return math.floor(f * 10 ** 1) / 10 ** 1
+
+def muendliche_berechnen(faecher, notentabelle):
+    for fach in faecher:
+        while True:
+            try:
+                muendlichenote = float(input(f"Note im Fach {fach}: "))
+                if 1.0 <= muendlichenote <= 5.0:
+                    notentabelle[fach]["muendlich"] = truncate(muendlichenote)
+                    break
+                else: 
+                    raise ValueError
+            except ValueError:
+                print("Die Note soll eine Zahl zwischen 1 und 5 sein.")
+    return notentabelle
+
+def gesamt_berechnen(notentabelle):
     muendliche = []
     for fach in notentabelle:
         if notentabelle[fach]["schriftlich"] > 0.0:
-            notentabelle[fach]["gesamt"] =  (notentabelle[fach]["semesternote"] + notentabelle[fach]["schriftlich"]) / 2
+            notentabelle[fach]["gesamt"] =  truncate((notentabelle[fach]["semesternote"] + notentabelle[fach]["schriftlich"]) / 2)
             if notentabelle[fach]["gesamt"] > 4.0:
-                muendliche.append(notentabelle[fach])
+                muendliche.append(fach)
                 print(f"Sie haben die FSP im Fach {fach} nicht bestanden und müssen Mündliche Prüfung bestehen!")
             if abs(notentabelle[fach]["semesternote"] - notentabelle[fach]["schriftlich"]) >= 1.0:
-                muendliche.append(notentabelle[fach])
+                muendliche.append(fach)
                 print(f"Es gibt eine Abweichung zwischen Semesternote und Prüfungsnote im {fach} von mehr als einer Note!")
         else:
             notentabelle[fach]["gesamt"] =  notentabelle[fach]["semesternote"]
-    muendliche_berechnen(muendliche, notentabelle)
-    return notentabelle
+    print(f"Sie müssen eine mündliche Prüfung in Fächern {", ".join(muendliche)} ablegen.")
+    return muendliche_berechnen(muendliche, notentabelle)
+
+
 def main():
     noten = {
         "Deutsch": {"semesternote": 0.0, "schriftlich": 0.0, "muendlich": 0.0, "gesamt": 0.0},
@@ -29,7 +47,7 @@ def main():
             try:
                 semesternote = float(input(f"Note im Fach {fach}: "))
                 if 1.0 <= semesternote <= 5.0:
-                    noten[fach]["semesternote"] = semesternote
+                    noten[fach]["semesternote"] = truncate(semesternote)
                     break
                 else: 
                     raise ValueError
@@ -38,25 +56,28 @@ def main():
     n = 0
     while n != 1:
         fsp_geschrieben = input("In welchen Fächer haben sie FSP geschrieben? ").replace(" ", "").split(",")
-        if not "Deutsch" in fsp_geschrieben or not "Mathematik" in fsp_geschrieben or ("Informatik" and "Physik" in fsp_geschrieben):
+        if ("Deutsch" not in fsp_geschrieben or "Mathematik" not in fsp_geschrieben) or ("Informatik" in fsp_geschrieben and "Physik" in fsp_geschrieben):
                     print("Sie müssen Deutsch und Mathematik als Pflichtfach schriftlich bestehen und können nicht gleichzeitig Informatik und Physik schreiben.")
         else:
             for fach in fsp_geschrieben:
                 if isinstance(fach, str) and fach in [fach for fach in noten] and len(fsp_geschrieben) == 3:
-                    
-                        while True:
-                            try:
-                                schriftlichenote = float(input(f"Schriftliche FSP im Fach {fach}: "))
-                                if 1.0 <= schriftlichenote <= 5.0:
-                                    noten[fach]["schriftlich"] = schriftlichenote
-                                    break
-                                else: 
-                                    raise ValueError
-                            except ValueError:
-                                print("Die Note soll eine Zahl zwischen 1 und 5 sein.")
-                        n = 1
-    print(berechnen(noten))
+                    while True:
+                        try:
+                            schriftlichenote = float(input(f"Schriftliche FSP im Fach {fach}: "))
+                            if 1.0 <= schriftlichenote <= 5.0:
+                                noten[fach]["schriftlich"] = truncate(schriftlichenote)
+                                break
+                            else: 
+                                raise ValueError
+                        except ValueError:
+                            print("Die Note soll eine Zahl zwischen 1 und 5 sein.")
+                    n = 1
+    print(gesamt_berechnen(noten))
         
 
 if __name__ == "__main__":
     main()
+
+# Abschneiden von Note: 4.27 zu 4.2 [+]
+# Nachklausuren
+# Eine verschieden Funktion für Input
